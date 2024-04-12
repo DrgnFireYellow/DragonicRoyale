9861b8ee4049df027def4abba2104a082ddf369d
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-04-12 11:06:49 -0400

    Fix many things

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,12 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.onarandombox.com/content/groups/public/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("com.onarandombox.multiversecore:multiverse-core:4.3.12")
 }
 
 java {
--- src/main/java/com/drgnfireyellow/dragonicroyale/DragonicRoyale.java
+++ src/main/java/com/drgnfireyellow/dragonicroyale/DragonicRoyale.java
@@ -2,13 +2,18 @@ package main.java.com.drgnfireyellow.dragonicroyale;
 
 import java.util.ArrayList;
 import java.io.File;
+// import java.io.IOException;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+// import org.apache.commons.io.FileUtils;
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.MultiverseCore.api.MVWorldManager;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
@@ -25,7 +30,9 @@ import org.bukkit.World;
 
 public class DragonicRoyale extends JavaPlugin implements Listener {
 
-    public ArrayList<Player> players = new ArrayList<Player>();
+    ArrayList<String> players = new ArrayList<String>();
+    MultiverseCore mvCore = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+    MVWorldManager worldManager = mvCore.getMVWorldManager();
 
     @Override
     public void onEnable() {
@@ -35,17 +42,28 @@ public class DragonicRoyale extends JavaPlugin implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
         Player player = event.getPlayer();
-        if (players.contains(player)) {
+        if ((players.contains(player.getName())) & (player.getWorld().getName().equals("dragonicroyale"))) {
             player.sendMessage("You lost...");
-            players.remove(player);
+            players.remove(player.getName());
+            player.spigot().respawn();
+            player.teleport(Bukkit.getWorlds().get(0).getSpawnLocation());
             if (players.size() == 1) {
-                Player winner = players.get(0);
+                Player winner = Bukkit.getPlayer(players.get(0));
                 winner.showTitle(Title.title(Component.text("You Win!"), Component.text("")));
                 winner.teleport(Bukkit.getWorlds().get(0).getSpawnLocation());
-                players.remove(winner);
-                File worldFolder = Bukkit.getWorld("dragonicroyale").getWorldFolder();
-                Bukkit.unloadWorld("dragonicroyale", false);
-                worldFolder.delete();
+                players.remove(winner.getName());
+                worldManager.regenWorld("dragonicroyale", true, true, null);
+                // File worldFolder = Bukkit.getWorld("dragonicroyale").getWorldFolder();
+                // for (Player p : Bukkit.getWorld("dragonicroyale").getPlayers()) {
+                //     player.teleport(Bukkit.getWorlds().get(0).getSpawnLocation());
+                // }
+                // System.out.println(Bukkit.unloadWorld(Bukkit.getWorld("dragonicroyale"), false));
+                // try {
+                //     FileUtils.deleteDirectory(worldFolder);
+                // }
+                // catch(IOException exception) {
+                //     Bukkit.getLogger().warning("Unable to delete world folder!");
+                // }
             }
         }
     }
@@ -53,29 +71,41 @@ public class DragonicRoyale extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             if (command.getName().equals("battleroyale")) {
-                players.add((Player) sender);
-                if (players.size() == 1) {
-                    sender.sendMessage("You are the host and can decide when to start the game.");
-                    WorldCreator mapCreator = new WorldCreator("dragonicroyale");
-                    mapCreator.type(WorldType.NORMAL);
-                    mapCreator.createWorld();
-                    World map = Bukkit.getWorld("dragonicroyale");
-                    map.getWorldBorder().setSize(100);
+                if (!players.contains(sender.getName())) {
+                    players.add(((Player) sender).getName());
+                    if (players.size() == 1) {
+                        sender.sendMessage("You are the host and can decide when to start the game.");
+                        if (worldManager.getMVWorld("dragonicroyale") == null) {
+                            worldManager.addWorld("dragonicroyale", World.Environment.NORMAL, null, WorldType.NORMAL, false, null);
+                            worldManager.getMVWorld("dragonicroyale").setHidden(true);
+                            Bukkit.getWorld("dragonicroyale").getWorldBorder().setSize(100);
+                            Bukkit.getWorld("dragonicroyale").setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true);
+                        }
+                    }
+                    else {
+                        sender.sendMessage("Please wait for the host to start the game...");
+                    }
                 }
                 else {
-                    sender.sendMessage("Please wait for the host to start the game...");
+                    sender.sendMessage("You are already in Battle Royale.");
                 }
                 return false;
             }
             if (command.getName().equals("startbattleroyale")) {
-                if (sender.equals(players.get(0))) {
-                    for (Player player : players) {
-                        player.getInventory().clear();
-                        ItemStack[] armor = {new ItemStack(Material.LEATHER_BOOTS), new ItemStack(Material.LEATHER_LEGGINGS), new ItemStack(Material.LEATHER_CHESTPLATE), new ItemStack(Material.LEATHER_HELMET)};
-                        player.getInventory().setArmorContents(armor);
-                        player.getInventory().addItem(new ItemStack(Material.STONE_SWORD));
-                        player.getInventory().addItem(new ItemStack(Material.STONE_PICKAXE));
-                        player.teleport(Bukkit.getWorld("dragonicroyale").getSpawnLocation());
+                if (sender.getName().equals(players.get(0))) {
+                    if (players.size() >= 2) {
+                        for (String playerName : players) {
+                            Player player = Bukkit.getPlayer(playerName);
+                            player.getInventory().clear();
+                            ItemStack[] armor = {new ItemStack(Material.LEATHER_BOOTS), new ItemStack(Material.LEATHER_LEGGINGS), new ItemStack(Material.LEATHER_CHESTPLATE), new ItemStack(Material.LEATHER_HELMET)};
+                            player.getInventory().setArmorContents(armor);
+                            player.getInventory().addItem(new ItemStack(Material.STONE_SWORD));
+                            player.getInventory().addItem(new ItemStack(Material.STONE_PICKAXE));
+                            player.teleport(Bukkit.getWorld("dragonicroyale").getSpawnLocation());
+                        }
+                    }
+                    else {
+                        sender.sendMessage("You need at least 2 people to play!");
                     }
                 }
                 else {
